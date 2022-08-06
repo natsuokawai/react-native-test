61aae03b5d5173a278e94e681edf808e00f16150
natsuokawai <kawai.natsuo@gmail.com>
2022-08-06 23:34:17 +0900

    Add message list

---
+++ src/App.tsx
@@ -0,0 +1,74 @@
+import * as React from "react";
+import {
+  SafeAreaView,
+  View,
+  FlatList,
+  StyleSheet,
+  Text,
+  StatusBar,
+} from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+type Message = {
+  id: number;
+  fromName: string;
+  content: string;
+};
+
+const data = Array.from(Array(10), (_, k) => k).map((num) => {
+  return {
+    id: num,
+    fromName: `user-${num}`,
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim",
+  } as Message;
+});
+
+const Item = ({ message }: { message: Message }) => (
+  <View style={styles.item}>
+    <Text>{message.fromName}</Text>
+    <Text>{message.content}</Text>
+  </View>
+);
+
+const HomeScreen = () => {
+  const renderItem = ({ item }: { item: Message }) => <Item message={item} />;
+  return (
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={data}
+        renderItem={renderItem}
+        keyExtractor={(item) => String(item.id)}
+      />
+    </SafeAreaView>
+  );
+};
+
+const Stack = createNativeStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight || 0,
+    backgroundColor: "white",
+  },
+  item: {
+    padding: 20,
+    marginVertical: 8,
+    borderBottomWidth: 1,
+    borderBottomColor: "rgb(239, 243, 244)",
+  },
+});
+
+export default App;
